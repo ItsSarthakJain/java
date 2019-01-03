3bca00347aa68b40ea23255c79475c3748bedbb3
sarthak1775 <me.sarthakjain@gmail.com>
2019-01-04 02:30:15 +0531

    array

---
+++ avg_mrks.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+	Scanner sc=new Scanner(System.in);
+      String s=sc.next();
+    int i=sc.nextInt(),j=sc.nextInt(),l=sc.nextInt();
+        int avg=(i+j+l)/3;
+System.out.println(s);
+System.out.println(avg);      
+		
+	}
+
+}
