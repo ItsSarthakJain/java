fb254a86eb878602cabfad58e8ed6d02fb5fad2c
sarthak1775 <me.sarthakjain@gmail.com>
2019-01-18 02:30:15 +0531

    C:/Program Files/Git/java

---
+++ charandBoolean/src/com/sarthak/java/Main.java
@@ -0,0 +1,14 @@
+package com.sarthak.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //width of char is (2 bytes)
+        char mychar = '\u00A9';
+        System.out.println("the unicode value is =" + mychar);
+        boolean myboolean = false;
+
+
+    }
+}
