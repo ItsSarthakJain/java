1d3aed5ac366ff0aa7a32c34cdcce4d8734639fd
sarthak1775 <me.sarthakjain@gmail.com>
2019-01-26 02:30:15 +0531

    C:/Program Files/Git/java

---
+++ floatanddouble/src/com/sarthak/java/Main.java
@@ -0,0 +1,14 @@
+package com.sarthak.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myintvalue=5/2;
+        float myfloatvalue=5f/3f;
+        double mydoublevalue=5d/3d;
+        System.out.println("myintvalue=" +myintvalue);
+        System.out.println("myfloatvalue=" +myfloatvalue);
+        System.out.println("mydoublevalue=" +mydoublevalue);
+        // write your code here
+    }
+}
---
+++ floatanddouble/src/convert.java
@@ -0,0 +1,8 @@
+public class convert {
+    public static void main(String[] args)
+    {
+        int pound=2;
+        double kg=(pound*0.45359237);
+        System.out.println("kilogram=" +kg);
+    }
+}
