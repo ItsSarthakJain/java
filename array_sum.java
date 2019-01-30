6676e697363be5b16a3d572b766ddbec25e2ada8
sarthak jain <31597446+sarthak1775@users.noreply.github.com>
2019-01-30 23:07:31 +0530

    Create array_sum.java

---
+++ array_sum.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+	public static int sum(int[] input){
+		
+      int n=input.length,sum=0;
+      for(int i=0;i<n;i++)
+      sum+=input[i];
+        return sum;
+	}
+	
+}
