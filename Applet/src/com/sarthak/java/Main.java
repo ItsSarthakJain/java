e579d6bf4ce6ba96a43ce21f813e9e75ff1d2364
sarthak1775 <me.sarthakjain@gmail.com>
2019-02-02 02:30:15 +0531

    C:/Program Files/Git/java

---
+++ Applet/src/com/sarthak/java/Main.java
@@ -0,0 +1,12 @@
+package com.sarthak.java;
+import java.applet.Applet;
+import java.awt.*;
+/*<applet code="Main" width=300 height=300>
+</applet>
+ */
+public class Main extends Applet {
+    public void paint(Graphics g)
+    {
+        g.drawString("welcome in java",100,100);
+    }
+}
---
+++ Applet/src/com/sarthak/java/Myapplet.java
@@ -0,0 +1,12 @@
+package com.sarthak.java;
+import java.awt.*;
+import java.applet.*;
+/*<applet code="Myapplet" width=300 height=300>
+</applet>
+ */
+public class Myapplet {
+    public void paint(Graphics g)
+    {
+        g.drawString("welcome in java",100,100);
+    }
+}
